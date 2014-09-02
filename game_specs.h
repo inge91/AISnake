d001f258773601d224feac135cd2063dbf3b43d0
inge91 <inge.becht91@gmail.com>
2014-09-02 18:05:56 +0200

    Changed playing field

--- game_specs.h
+++ game_specs.h
@@ -1,7 +1,7 @@
 #ifndef GAME_SPECS_H
 #define GAME_SPECS_H
-#define WINDOW_WIDTH  200
-#define WINDOW_HEIGHT 200
+#define WINDOW_WIDTH  240
+#define WINDOW_HEIGHT 240
 #define FRAME_RATE 30
 // how many ms between each movement
 #define SNAKE_SPEED 200
